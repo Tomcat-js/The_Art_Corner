b7b074cce7a329fa771bf50caf737a019091807f
Tom <thomascatchlove@gmail.com>
2020-09-07 10:29:12 +1000

    Use production config from Heroku ENV variables

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -12,7 +12,7 @@ database_url =
     """
 
 config :art_page, ArtPage.Repo,
-  # ssl: true,
+  ssl: true,
   url: database_url,
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
